417a18a04cd45a6c021971a7467d0517db155200
Martin Richard Skovdal <martinrichardskovdal@MRS-MacBook-Pro.local>
2020-10-03 09:32:20 +0200

    -

---
+++ systemStorages/prepareDefaultDatabaseSingle/modal.php
@@ -0,0 +1,242 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/shared/required/requiredStart.php';
+
+$validateFlag = 200;
+
+$configuration_deleteConfirmationSystemStorages = getSystemConfigurations('deleteConfirmationSystemStorages');
+if($configuration_deleteConfirmationSystemStorages == -1){
+	$configuration_deleteConfirmationSystemStorages = 1;
+}
+
+if(isset($_POST['modalId']) === false){
+	$validateFlag = 400;
+}
+else{
+	$modalId = $_POST['modalId'];
+}
+
+if(isset($_POST['systemStorages_id']) === false){
+	$validateFlag = 400;
+}
+else{
+	$systemStorages_id = $_POST['systemStorages_id'];
+	if(decodeId($systemStorages_id) == -1){
+		$validateFlag = 400;
+	}
+	else{
+		$systemStorages_id = decodeId($systemStorages_id);
+	}
+}
+
+if(isset($con) === false){$con = dbConnection();}
+$stmt = $con->stmt_init();
+$stmt->prepare("
+	SELECT
+		`c0`.`systemStorages`.`name` AS `systemStorages_name`
+	FROM
+		`c0`.`systemStorages`
+	WHERE
+		`c0`.`systemStorages`.`id` = ?
+	LIMIT 1
+");
+$stmt->bind_param('i', $systemStorages_id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if(mysqli_num_rows($result) == 0){
+	$validateFlag = 400;
+}
+else{
+	while($row = mysqli_fetch_assoc($result)){
+		$systemStorages_name = $row['systemStorages_name'];
+	}
+}
+$result->close();
+
+if($validateFlag == 200){
+?>
+	<h1>Klargør systemlager til at være standarddatabase</h1>
+	<form action="/systemStorages/prepareDefaultDatabaseSingle/modalSubmit.php" enctype="application/x-www-form-urlencoded" method="post" onsubmit="submitForm(this);" target="<?php echo md5($_SERVER['SCRIPT_FILENAME']) . purify($modalId); ?>">
+		<iframe name="<?php echo md5($_SERVER['SCRIPT_FILENAME']) . purify($modalId); ?>" src="about:blank"></iframe>
+		<input name="modalId" type="hidden" value="<?php echo purify($modalId); ?>">
+		<input name="systemStorages_id" type="hidden" value="<?php echo encodeId(purify($systemStorages_id)); ?>">
+		<div>
+			Bekræft venligst at du ønsker at klargøre systemlageret <strong><?php echo purify($systemStorages_name); ?></strong> til at være standarddatabase for systemet.<br>
+			<br>
+			<strong>VIGTIGT!<br>
+			Alle data på systemlageret slettes, inden klargøringen påbegyndes. Handlingen kan ikke fortrydes.</strong>
+		</div>
+		<div class="buttons">
+			<input class="close" onclick="document.querySelectorAll('#modal-<?php echo purify($modalId); ?> div.close')[0].click();" type="button" value="Luk"><input class="delete" type="submit" value="Klargør systemlager">
+		</div>
+	</form>
+	<?php
+	if(getSystemConfigurations('logSystemStorages') == 1 || getSystemConfigurations('logSystemStorages') == -1){
+		$type = 'view';
+		$trigger_systemUsers_id = $_SESSION['systemUsers_id'];
+		$ipAddress = $_SERVER['REMOTE_ADDR'];
+		$startTime = $systemEvents_addEvent_startTime;
+		$endTime = microtime(true);
+		$latency = ($endTime - $startTime);
+		$finished = 0;
+		$host = $_SERVER['SERVER_NAME'];
+		$httpVersion = $_SERVER['SERVER_PROTOCOL'];
+		$instanceId = session_id();
+		$method = $_SERVER['REQUEST_METHOD'];
+		$status = $validateFlag;
+		$userAgent = $_SERVER['HTTP_USER_AGENT'];
+		$version = date('YmdHisu', filemtime(__FILE__));
+		$postData = json_encode($_POST);
+		$getData = json_encode($_GET);
+		$httpsEnabled = $_SERVER['HTTPS'];
+		$fileName = $_SERVER['SCRIPT_FILENAME'];
+		$devices_id = null;
+		$exports_id = null;
+		$filesContent_id = null;
+		$filesMetaData_id = null;
+		$identities_id = null;
+		$meetings_id = null;
+		$policies_id = null;
+		$processingActivities_id = null;
+		$processingActivitiesLegalBasises_id = null;
+		$progressTime_id = null;
+		$pseudoNames_id = null;
+		$sessions_id = null;
+		$systemConfigurations_id = null;
+		$systemEvents_id = null;
+		$systemNotifications_id = null;
+		$systemStorages_id = null;
+		$systemUsers_id = null;
+		$systemUsersSystemPreferences_id = null;
+		$tableVersions_id = null;
+		$tags_id = null;
+		$tagsReferences_id = null;
+		
+		addSystemEvent(
+			$trigger_systemUsers_id,
+			$type,
+			$ipAddress,
+			$startTime,
+			$endTime,
+			$finished,
+			$host,
+			$httpVersion,
+			$instanceId,
+			$method,
+			$status,
+			$userAgent,
+			$version,
+			$postData,
+			$getData,
+			$httpsEnabled,
+			$fileName,
+			$devices_id,
+			$exports_id,
+			$filesContent_id,
+			$filesMetaData_id,
+			$identities_id,
+			$meetings_id,
+			$policies_id,
+			$processingActivities_id,
+			$processingActivitiesLegalBasises_id,
+			$progressTime_id,
+			$pseudoNames_id,
+			$sessions_id,
+			$systemConfigurations_id,
+			$systemEvents_id,
+			$systemNotifications_id,
+			$systemStorages_id,
+			$systemUsers_id,
+			$systemUsersSystemPreferences_id,
+			$tableVersions_id,
+			$tags_id,
+			$tagsReferences_id
+		);
+	}
+}
+else{
+	if(getSystemConfigurations('logSystemStorages') == 1 || getSystemConfigurations('logSystemStorages') == -1){
+		$type = 'view';
+		$trigger_systemUsers_id = $_SESSION['systemUsers_id'];
+		$ipAddress = $_SERVER['REMOTE_ADDR'];
+		$startTime = $systemEvents_addEvent_startTime;
+		$endTime = microtime(true);
+		$latency = ($endTime - $startTime);
+		$finished = 0;
+		$host = $_SERVER['SERVER_NAME'];
+		$httpVersion = $_SERVER['SERVER_PROTOCOL'];
+		$instanceId = session_id();
+		$method = $_SERVER['REQUEST_METHOD'];
+		$status = $validateFlag;
+		$userAgent = $_SERVER['HTTP_USER_AGENT'];
+		$version = date('YmdHisu', filemtime(__FILE__));
+		$postData = json_encode($_POST);
+		$getData = json_encode($_GET);
+		$httpsEnabled = $_SERVER['HTTPS'];
+		$fileName = $_SERVER['SCRIPT_FILENAME'];
+		$devices_id = null;
+		$exports_id = null;
+		$filesContent_id = null;
+		$filesMetaData_id = null;
+		$identities_id = null;
+		$meetings_id = null;
+		$policies_id = null;
+		$processingActivities_id = null;
+		$processingActivitiesLegalBasises_id = null;
+		$progressTime_id = null;
+		$pseudoNames_id = null;
+		$sessions_id = null;
+		$systemConfigurations_id = null;
+		$systemEvents_id = null;
+		$systemNotifications_id = null;
+		$systemStorages_id = null;
+		$systemUsers_id = null;
+		$systemUsersSystemPreferences_id = null;
+		$tableVersions_id = null;
+		$tags_id = null;
+		$tagsReferences_id = null;
+		
+		addSystemEvent(
+			$trigger_systemUsers_id,
+			$type,
+			$ipAddress,
+			$startTime,
+			$endTime,
+			$finished,
+			$host,
+			$httpVersion,
+			$instanceId,
+			$method,
+			$status,
+			$userAgent,
+			$version,
+			$postData,
+			$getData,
+			$httpsEnabled,
+			$fileName,
+			$devices_id,
+			$exports_id,
+			$filesContent_id,
+			$filesMetaData_id,
+			$identities_id,
+			$meetings_id,
+			$policies_id,
+			$processingActivities_id,
+			$processingActivitiesLegalBasises_id,
+			$progressTime_id,
+			$pseudoNames_id,
+			$sessions_id,
+			$systemConfigurations_id,
+			$systemEvents_id,
+			$systemNotifications_id,
+			$systemStorages_id,
+			$systemUsers_id,
+			$systemUsersSystemPreferences_id,
+			$tableVersions_id,
+			$tags_id,
+			$tagsReferences_id
+		);
+	}
+}
+require_once $_SERVER['DOCUMENT_ROOT'] . '/shared/required/requiredEnd.php';
+?>
