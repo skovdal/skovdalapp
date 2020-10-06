8a9b7af74d692f67013b219719865efad6210de0
Martin Richard Skovdal <martinrichardskovdal@MRS-MacBook-Pro.local>
2020-10-06 21:20:58 +0200

    -

--- systemStorages/add/modal.php
+++ systemStorages/add/modal.php
@@ -90,7 +90,7 @@ if($validateFlag == 200){
 		</div>
 		
 		<div class="buttons">
-			<div class="pulseContainer"><div class="pulseCore success"></div><div class="pulse success"></div></div>
+			<div class="pulseContainer"><div class="pulseCore danger"></div><div class="pulse danger"></div></div>
 			<input class="close" onclick="document.querySelectorAll('#modal-<?php echo purify($modalId); ?> div.close')[0].click();" type="button" value="Luk"><input type="submit" value="Gem systemlager">
 		</div>
 	</form>
