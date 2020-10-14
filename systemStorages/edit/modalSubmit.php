afdd00ff8ca8ca319fd26e535a476b62ebd4853f
Martin Richard Skovdal <martinrichardskovdal@MRS-MacBook-Pro.local>
2020-10-14 23:17:30 +0200

    -

--- systemStorages/edit/modalSubmit.php
+++ systemStorages/edit/modalSubmit.php
@@ -3,8 +3,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/shared/required/requiredStart.php';
 
 $validateFlag = 200;
 
-$configuration_logSystemUsers = getSystemConfigurations('logSystemUsers');
-if($configuration_logSystemUsers == -1){
+$configuration_logSystemStorages = getSystemConfigurations('logSystemStorages');
+if($configuration_logSystemStorages == -1){
 	$validateFlag = 400;
 }
 
@@ -15,16 +15,16 @@ else{
 	$modalId = $_POST['modalId'];
 }
 
-if(isset($_POST['systemUsers_id']) === false){
+if(isset($_POST['systemStorages_id']) === false){
 	$validateFlag = 400;
 }
 else{
-	$systemUsers_id = $_POST['systemUsers_id'];
-	if(decodeId($systemUsers_id) == -1){
+	$systemStorages_id = $_POST['systemStorages_id'];
+	if(decodeId($systemStorages_id) == -1){
 		$validateFlag = 400;
 	}
 	else{
-		$systemUsers_id = decodeId($systemUsers_id);
+		$systemStorages_id = decodeId($systemStorages_id);
 	}
 }
 
@@ -84,20 +84,20 @@ if($validateFlag == 200){
 	$stmt = $con->stmt_init();
 	$stmt->prepare("
 		UPDATE
-			`c0`.`systemUsers`
+			`c0`.`systemStorages`
 		SET
-			`c0`.`systemUsers`.`name` = ?,
-			`c0`.`systemUsers`.`securityClearance` = ?,
-			`c0`.`systemUsers`.`blockSignIn` = ?,
-			`c0`.`systemUsers`.`userMustChangePasswordAtNextSignIn` = ?,
-			`c0`.`systemUsers`.`forceUseOfMultifactorAuthentication` = ?,
-			`c0`.`systemUsers`.`identities_id` = ?
+			`c0`.`systemStorages`.`name` = ?,
+			`c0`.`systemStorages`.`securityClearance` = ?,
+			`c0`.`systemStorages`.`blockSignIn` = ?,
+			`c0`.`systemStorages`.`userMustChangePasswordAtNextSignIn` = ?,
+			`c0`.`systemStorages`.`forceUseOfMultifactorAuthentication` = ?,
+			`c0`.`systemStorages`.`identities_id` = ?
 		WHERE
-			`c0`.`systemUsers`.`id` = ?
+			`c0`.`systemStorages`.`id` = ?
 	");
-	$stmt->bind_param('ssiiiii', $name, $securityClearance, $blockSignIn, $userMustChangePasswordAtNextSignIn, $forceUseOfMultifactorAuthentication, $identities_id, $systemUsers_id);
+	$stmt->bind_param('ssiiiii', $name, $securityClearance, $blockSignIn, $userMustChangePasswordAtNextSignIn, $forceUseOfMultifactorAuthentication, $identities_id, $systemStorages_id);
 	$stmt->execute();
-	setTableVersion('systemUsers');
+	setTableVersion('systemStorages');
 	
 	if(isset($con) === false){$con = dbConnection();}
 	$stmt = $con->stmt_init();
@@ -107,9 +107,9 @@ if($validateFlag == 200){
 		SET
 			`s0`.`tagsReferences`.`legacyFlag` = 1
 		WHERE
-			`s0`.`tagsReferences`.`systemUsers_id` = ?
+			`s0`.`tagsReferences`.`systemStorages_id` = ?
 	");
-	$stmt->bind_param('i', $systemUsers_id);
+	$stmt->bind_param('i', $systemStorages_id);
 	$stmt->execute();
 	setTableVersion('tagsReferences');
 	
@@ -185,7 +185,7 @@ if($validateFlag == 200){
 					`s0`.`tagsReferences`
 				(
 					`s0`.`tagsReferences`.`tags_id`,
-					`s0`.`tagsReferences`.`systemUsers_id`
+					`s0`.`tagsReferences`.`systemStorages_id`
 				)
 				VALUES
 				(
@@ -193,7 +193,7 @@ if($validateFlag == 200){
 					?
 				)
 			");
-			$stmt->bind_param('ii', $tags_id, $systemUsers_id);
+			$stmt->bind_param('ii', $tags_id, $systemStorages_id);
 			$stmt->execute();
 			setTableVersion('tagsReferences');
 		}
@@ -205,7 +205,7 @@ if($validateFlag == 200){
 		parent.toastr('success', 'Editer systembruger', 'Systembrugeren blev editeret.', 0, true, '');
 	</script>
 	<?php
-	if(getSystemConfigurations('logSystemUsers') == 1 || getSystemConfigurations('logSystemUsers') == -1){
+	if(getSystemConfigurations('logSystemStorages') == 1 || getSystemConfigurations('logSystemStorages') == -1){
 		$type = 'edit';
 		$trigger_systemUsers_id = $_SESSION['systemUsers_id'];
 		$ipAddress = $_SERVER['REMOTE_ADDR'];
@@ -300,7 +300,7 @@ else{
 		parent.toastr('danger', 'Der er opstået en fejl!', 'Der er desværre opstået en fejl i systemet, hvilket vi beklager.<br><br>Fejlen er rapporteret og vil blive adresseret i løbet af kort tid.<br><br>Klik her for at følge status...', 0, true, 'https://errors.complian.app.complian.dev?1234-ABCD-5678-EFGH');
 	</script>
 	<?php
-	if(getSystemConfigurations('logSystemUsers') == 1 || getSystemConfigurations('logSystemUsers') == -1){
+	if(getSystemConfigurations('logSystemStorages') == 1 || getSystemConfigurations('logSystemStorages') == -1){
 		$type = 'edit';
 		$trigger_systemUsers_id = $_SESSION['systemUsers_id'];
 		$ipAddress = $_SERVER['REMOTE_ADDR'];
