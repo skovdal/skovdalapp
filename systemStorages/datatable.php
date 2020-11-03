02ba7acef590820828c90cab9d09a21486db4e38
Martin Richard Skovdal <martinrichardskovdal@MRS-MacBook-Pro.local>
2020-11-03 15:12:32 +0100

    -

--- systemStorages/datatable.php
+++ systemStorages/datatable.php
@@ -335,7 +335,7 @@ if($validateFlag == 200){
 				<td class="checkbox unchecked" onclick="datatableCheckbox(this);">
 					<input name="systemStorages_id" type="checkbox" value="<?php echo encodeId(purify($row['systemStorages_id'])); ?>">
 				</td>
-				<td>
+				<td style="position:relative;">
 					<div class="pulseContainer" id="pulseContainer<?php echo $datatableId; ?><?php echo encodeId(purify($row['systemStorages_id'])); ?>"><div class="pulseCore danger"></div><div class="pulse danger"></div></div>
 					
 					<input id="inputType<?php echo $datatableId; ?><?php echo encodeId(purify($row['systemStorages_id'])); ?>" type="hidden" value="<?php echo purify($row['systemStorages_type']); ?>">
