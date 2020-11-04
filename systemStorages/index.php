7ace977a45540be1451b86cf9e9fd529f3f9509b
Martin Richard Skovdal <martinrichardskovdal@MRS-MacBook-Pro.local>
2020-11-04 08:21:33 +0100

    -

--- systemStorages/index.php
+++ systemStorages/index.php
@@ -509,12 +509,12 @@ if($validateFlag == 200){
 									if('ConnectionStatus' == orderBy('orderBy', $preferences_orderBySystemStorages_orderBy)){
 										if(orderBy('orderBySort', $preferences_orderBySystemStorages_orderBy) == 'ASC'){
 										?>
-											<th onclick="datatableOrderBy('connectionStatus', this, this.closest('div.datatable').id);" style="background-image:url('/images/svgImage.php?id=<?php echo urlencode('/images/fontawesome-pro-5.9.0-web/svgs/light/sort-amount-up.svg'); ?>&fill=<?php echo urlencode('rgba(135,140,145,1)'); ?>'); padding:20px 36px 20px 10px; <?php if($preferences_columnsSystemStorages_columnsConnectionStatus == 1){}else{echo 'display:none;';} ?>">
+											<th onclick="datatableOrderBy('connectionStatus', this, this.closest('div.datatable').id);" style="background-image:url('/images/svgImage.php?id=<?php echo urlencode('/images/fontawesome-pro-5.9.0-web/svgs/light/sort-amount-up.svg'); ?>&fill=<?php echo urlencode('rgba(135,140,145,1)'); ?>'); background-position:center center; padding:20px 36px 20px 10px; <?php if($preferences_columnsSystemStorages_columnsConnectionStatus == 1){}else{echo 'display:none;';} ?>">
 										<?php
 										}
 										else{
 										?>
-											<th onclick="datatableOrderBy('connectionStatus', this, this.closest('div.datatable').id);" style="background-image:url('/images/svgImage.php?id=<?php echo urlencode('/images/fontawesome-pro-5.9.0-web/svgs/light/sort-amount-down.svg'); ?>&fill=<?php echo urlencode('rgba(135,140,145,1)'); ?>'); padding:20px 36px 20px 10px; <?php if($preferences_columnsSystemStorages_columnsConnectionStatus == 1){}else{echo 'display:none;';} ?>">
+											<th onclick="datatableOrderBy('connectionStatus', this, this.closest('div.datatable').id);" style="background-image:url('/images/svgImage.php?id=<?php echo urlencode('/images/fontawesome-pro-5.9.0-web/svgs/light/sort-amount-down.svg'); ?>&fill=<?php echo urlencode('rgba(135,140,145,1)'); ?>'); background-position:center center; padding:20px 36px 20px 10px; <?php if($preferences_columnsSystemStorages_columnsConnectionStatus == 1){}else{echo 'display:none;';} ?>">
 										<?php
 										}
 									}
@@ -524,7 +524,6 @@ if($validateFlag == 200){
 									<?php
 									}
 									?>
-										<img height="15" src="/images/svgImage.php?id=<?php echo urlencode('/images/fontawesome-pro-5.9.0-web/svgs/light/heart-rate.svg'); ?>&fill=<?php echo urlencode('rgba(235,240,245,1)'); ?>">
 									</th>
 									<?php
 									if('Name' == orderBy('orderBy', $preferences_orderBySystemStorages_orderBy)){
